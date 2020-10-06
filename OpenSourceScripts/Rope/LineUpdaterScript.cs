fcb3ae75d7e278c0e3cc5fcd24dae735c9ed0d23
Caner Yılık <programlamakitabi@gmail.com>
2020-10-06 11:59:50 +0300

    Add files via upload

---
+++ OpenSourceScripts/Rope/LineUpdaterScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineUpdaterScript : MonoBehaviour
+{
+    public GameObject parentObj;
+    public GameObject mesh;
+    public int lineCount;
+
+    public Material meshMaterial;
+    LineRenderer ropeMesh;
+
+
+
+    private void Start()
+    {
+        ropeMesh = this.GetComponent<LineRenderer>();
+
+    }
+    public void CreateMesh()
+    {
+       
+        mesh.name = "RopeMesh";
+
+        //mesh.AddComponent<LineUpdaterScript>();
+
+        ropeMesh = mesh.AddComponent<LineRenderer>();
+
+        MeshCollider ropemeshCollider = mesh.AddComponent<MeshCollider>();
+
+        ropemeshCollider.convex = true;
+        ropemeshCollider.isTrigger = true;
+
+        ropeMesh.positionCount = lineCount;
+        ropeMesh.startWidth = 2; ropeMesh.endWidth = 2;
+        ropeMesh.material = meshMaterial;
+        ropeMesh.numCapVertices = 90;
+
+        Mesh meshCreatedRope = new Mesh();
+        ropeMesh.BakeMesh(meshCreatedRope, true);
+        ropemeshCollider.sharedMesh = meshCreatedRope;
+    }
+
+    void DrawLineRenderer_Update()
+    {
+        for (int i = 0; i < lineCount; i++)
+        {
+            ropeMesh.SetPosition(i, parentObj.transform.GetChild(i).position);
+        }
+    }
+
+    private void Awake()
+    {
+        //CreateMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawLineRenderer_Update();
+    }
+}
---
+++ OpenSourceScripts/Rope/RopeCreator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeCreator : MonoBehaviour
+{
+    public GameObject parentObject;
+    public GameObject partPrefab;
+   // public GameObject presserPrefab;
+    public int ropePartCount;
+    public Material meshMaterial;
+
+    public void Spawn()
+    {
+        GameObject lineMesh = new GameObject();
+        LineUpdaterScript lineUpdater = lineMesh.AddComponent<LineUpdaterScript>();
+        lineUpdater.mesh=lineMesh;
+        lineUpdater.lineCount = ropePartCount;
+        lineUpdater.meshMaterial = meshMaterial;
+        lineUpdater.CreateMesh();
+
+
+        parentObject = SpawnEmptyParent();
+
+
+        GameObject firstPart = createParts(parentObject.transform);
+
+        firstPart.transform.eulerAngles = new Vector3(-90, 0, 0);
+
+        DestroyImmediate(firstPart.GetComponent<CharacterJoint>());
+
+        firstPart.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+
+
+        for (int i = 0; i < ropePartCount-1; i++)
+        {
+            GameObject ropeParts = createParts(parentObject.transform.GetChild(i));
+            ropeParts.transform.eulerAngles = new Vector3(-90, 0, 0);
+
+            ropeParts.GetComponent<CharacterJoint>().connectedBody = parentObject.transform.GetChild(i).GetComponent<Rigidbody>();
+        }
+
+        parentObject.transform.GetChild(parentObject.transform.childCount - 1).GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+        lineUpdater.parentObj = parentObject;
+
+        foreach (Transform ropePart in parentObject.transform)
+            ropePart.GetComponent<MeshRenderer>().enabled = false;
+        
+
+    }
+
+    GameObject createParts(Transform _obj)
+    {
+        return Instantiate(partPrefab, new Vector3(_obj.position.x, _obj.position.y, _obj.position.z + 2f), Quaternion.identity, parentObject.transform);
+    }
+    
+    GameObject SpawnEmptyParent()
+    {
+        GameObject ropeHolder = new GameObject();
+        ropeHolder.name = "Holder";
+        return ropeHolder;
+    }
+}
